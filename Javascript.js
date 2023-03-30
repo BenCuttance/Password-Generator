382a4ee26f28146cb95a9a0753dd4e5d0db4736f
BenC <bcuttance1998@gmail.com>
2023-03-30 17:34:28 +1030

    Removed code so that password generates. added code so that numbers will always display in password

--- Javascript.js
+++ Javascript.js
@@ -1,25 +1,62 @@
 // Arrays 
-var passwordLength = 8;
-var choice = [];
+var characterLength = 8;
+var choiceArr = [];
 // Characters to be used in password generation
 var special = ["!","@","#","$","%","^","&","*","(",")","?","<",">","/","[","]","{","}","|",";",":","=","+",];
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","d","t","u","v","w","x","y","z",];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var number = ["1","2","3","4","5","6","7","8","9","0",];
-
+var numbersExist = false
+var specialExist = false
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function getPrompts(){
+
+    choiceArr = [];    
+    characterLength = parseInt(prompt("How many characters in your new password? (8-100)"));
+    
+    if (isNaN(characterLength) || characterLength < 8 || characterLength > 100) {
+        alert("length between 8 - 100");
+        return false;
+    
+        }
+    
+        if (confirm("would you like lowercase letters to be included?")) {
+            choiceArr = choiceArr.concat(lower);
+        }
+    
+        if (confirm("would you like uppercase letters to be included?")) {
+            choiceArr = choiceArr.concat(upper);
+    
+        }
+    
+        if (confirm("would you like numbers to be included?")) {
+            numbersExist = true
+            choiceArr = choiceArr.concat(number);
+    
+        }
+            if (confirm("would you like special characters to be included?")) {
+                specialExist = true
+                choiceArr = choiceArr.concat(special);
+    
+        }
+    
+        return true;
+    }
+
 // Write password to the #password input
 function writePassword() {
     var correctPrompts = getPrompts();
     var passwordText = document.querySelector("#password");
     
+
     if (correctPrompts) {
         var newPassword = generatePassword();
+        
          passwordText.value = newPassword;
        
     }
@@ -28,44 +65,28 @@ function writePassword() {
 function generatePassword() {
 var password = "";
 
-    for (var i = 0; i < passwordLength.length; i++) {
-        var randomIndex = Math.floor(Math.random() * choice.length);
-        password = password + choice[randomIndex];
+    for (var i = 0; i < characterLength; i++) {
+        var randomIndex = Math.floor(Math.random() * choiceArr.length);
+        password = password + choiceArr[randomIndex];
 
     }     
+        //check if numbers were included 
+            //if they were included modified the first charcter to be a number
+    if (numbersExist){
 
-    return password;
+       password = password.replace(password[0], Math.floor(Math.random() * 10))
+        numbersExist = false
+    }
+
+    if (specialExist){
+
+        password = password.replace(password[1], Math.floor(Math.random() *special.length))
+         numbersExist = false
+     }
+
+    
+    
+            return password;
 }
 
 
-function getPrompts(){
-
-choice =[];    
-passwordLength = parseInt(prompt("How many characters in your new password? (Minimum 8)"));
-
-if (isNaN(passwordLength) || passwordLength < 8 ) {
-    alert("length insufficent");
-    return false;
-
-    }
-
-    if (confirm("would you like lowercase letters to be included?")) {
-        choice = choice.concat(lower);
-    }
-
-    if (confirm("would you like uppercase letters to be included?")) {
-        choice = choice.concat(upper);
-
-    }
-
-    if (confirm("would you like numbers to be included?")) {
-        choice = choice.concat(number);
-
-    }
-        if (confirm("would you like special characters to be included?")) {
-        choice = choice.concat(special);
-
-    }
-
-    return true;
-}
